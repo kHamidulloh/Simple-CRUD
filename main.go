10d1cd07daf2910a21d3a86b5a034fffb1550987
kHamidulloh <toxtayevhamidulloh6997@gmail.com>
2025-03-04 19:49:40 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	// "long"
+	// "encoding/json"
+	// "math/rand"
+	// "net/http"
+	// "strconv"
+	// "github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Printf("Hi")
+}
